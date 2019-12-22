6726118ebfa199af0478c0b123b6f70cdb10fd23
renuevo <renuevo0110@gmail.com>
2019-12-22 18:06:58 +0900

    Spring Boot Exception In Action Init :tada:

---
+++ spring-boot-exception-in-action/src/main/java/com/github/renuevo/SpringBootExceptionApplication.java
@@ -0,0 +1,13 @@
+package com.github.renuevo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExceptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExceptionApplication.class, args);
+    }
+
+}
---
+++ spring-boot-exception-in-action/src/main/java/com/github/renuevo/controller/Controller.java
@@ -0,0 +1,7 @@
+package com.github.renuevo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+}
