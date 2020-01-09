e72a8926fb7a25b6cbcde7c4e802fba0040d32c0
renuevo <renuevo0110@gmail.com>
2020-01-09 22:33:00 +0900

    Spring Boot Rest Client Init :tada:

---
+++ spring-boot-rest-client-in-action/README.md
@@ -0,0 +1,1 @@
+# HTTP Rest Call
---
+++ spring-boot-rest-client-in-action/src/main/java/com/github/renuevo/SpringBootRestClientApplication.java
@@ -0,0 +1,11 @@
+package com.github.renuevo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestClientApplication.class, args);
+    }
+}
