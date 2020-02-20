f9b2a10d6077ff970c95ff8c5a5a3ebbf0854b67
renuevo <0110kdh@naver.com>
2020-02-20 18:12:36 +0900

    Spring Boot Rest Client - Feign Setting Update

--- spring-boot-rest-client-in-action/src/main/java/com/github/renuevo/feign/SampleFeignClient.java
+++ spring-boot-rest-client-in-action/src/main/java/com/github/renuevo/feign/SampleFeignClient.java
@@ -3,9 +3,9 @@ package com.github.renuevo.feign;
 
 import com.github.renuevo.dto.NaverBlogParam;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.SpringQueryMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "Sample-Feign-Client", url = "https://openapi.naver.com/v1/search")
 public interface SampleFeignClient {
@@ -14,6 +14,6 @@ public interface SampleFeignClient {
     String naverBlogSearch(
             @RequestHeader("X-Naver-Client-Id") String id,
             @RequestHeader("X-Naver-Client-Secret") String secret,
-            @RequestParam NaverBlogParam naverBlogParam);
+            @SpringQueryMap NaverBlogParam naverBlogParam);
 
 }
