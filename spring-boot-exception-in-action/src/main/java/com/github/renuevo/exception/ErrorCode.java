bfe04624cf03e4cc4c6714562a4eb128f836d8cf
renuevo <0110kdh@naver.com>
2020-03-25 14:30:22 +0900

    Spring Boost Exception - Rest Controller Advice Default Setting  :sparkles:

--- spring-boot-exception-in-action/src/main/java/com/github/renuevo/exception/ErrorCode.java
+++ spring-boot-exception-in-action/src/main/java/com/github/renuevo/exception/ErrorCode.java
@@ -3,8 +3,13 @@ package com.github.renuevo.exception;
 import lombok.Getter;
 
 @Getter
-public enum ErrorCode {
-    REST_CONTROLLER_VALUE(400, "V001", "Invalid Input Value");
+public enum  ErrorCode {
+
+    // Common
+    INVALID_INPUT_VALUE(400, "C001", " Invalid Input Value"),
+    METHOD_NOT_ALLOWED(405, "C002", " Invalid Input Value"),
+    INTERNAL_SERVER_ERROR(500, "C003", "Server Error"),
+    INVALID_TYPE_VALUE(400, "C004", " Invalid Type Value");
 
     private final String code;
     private final String message;
@@ -12,7 +17,9 @@ public enum ErrorCode {
 
     ErrorCode(final int status, final String code, final String message) {
         this.status = status;
-        this.code = code;
         this.message = message;
+        this.code = code;
     }
+
+
 }
--- spring-boot-exception-in-action/src/main/java/com/github/renuevo/response/ErrorResponse.java
+++ spring-boot-exception-in-action/src/main/java/com/github/renuevo/response/ErrorResponse.java
@@ -1,13 +1,16 @@
 package com.github.renuevo.response;
 
 import com.github.renuevo.exception.ErrorCode;
+import com.google.common.collect.Lists;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-@Setter
 @Getter
 @NoArgsConstructor
 public class ErrorResponse {
@@ -17,18 +20,63 @@ public class ErrorResponse {
     private List<FieldError> errors;
     private String code;
 
-    @Getter
-    @NoArgsConstructor
-    public static  class FieldError{
-        private String field;
-        private String value;
-        private String reason;
+    private ErrorResponse(final ErrorCode code, final List<FieldError> errors) {
+        this.message = code.getMessage();
+        this.status = code.getStatus();
+        this.errors = errors;
+        this.code = code.getCode();
     }
 
-    public ErrorResponse(final ErrorCode code){
+    private ErrorResponse(final ErrorCode code) {
         this.message = code.getMessage();
         this.status = code.getStatus();
         this.code = code.getCode();
+        this.errors = Lists.newArrayList();
     }
+
+
+    public static ErrorResponse of(final ErrorCode code, final BindingResult bindingResult) {
+        return new ErrorResponse(code, FieldError.of(bindingResult));
+    }
+
+    public static ErrorResponse of(final ErrorCode code) {
+        return new ErrorResponse(code);
+    }
+
+    public static ErrorResponse of(final ErrorCode code, final List<FieldError> errors) {
+        return new ErrorResponse(code, errors);
+    }
+
+    public static ErrorResponse of(MethodArgumentTypeMismatchException e) {
+        final String value = e.getValue() == null ? "" : e.getValue().toString();
+        final List<ErrorResponse.FieldError> errors = ErrorResponse.FieldError.of(e.getName(), value, e.getErrorCode());
+        return new ErrorResponse(ErrorCode.INVALID_TYPE_VALUE, errors);
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class FieldError {
+        private String field;
+        private String value;
+        private String reason;
+
+        public static List<FieldError> of(final String field, final String value, final String reason) {
+            List<FieldError> fieldErrors = Lists.newArrayList();
+            fieldErrors.add(new FieldError(field, value, reason));
+            return fieldErrors;
+        }
+
+        private static List<FieldError> of(final BindingResult bindingResult) {
+            final List<org.springframework.validation.FieldError> fieldErrors = bindingResult.getFieldErrors();
+            return fieldErrors.stream()
+                    .map(error -> new FieldError(
+                            error.getField(),
+                            error.getRejectedValue() == null ? "" : error.getRejectedValue().toString(),
+                            error.getDefaultMessage()
+                    )).collect(Collectors.toList());
+        }
+    }
+
 
 }
