321ebf6c794260b513da9f531757e987dcf1a060
renuevo <renuevo0110@gmail.com>
2020-08-31 23:39:47 +0900

    Spring Guide - Spring Guide Init :tada:

---
+++ spring-boot-guide-in-action/src/main/java/com/github/renuevo/SpringBootGuideApplication.java
@@ -0,0 +1,13 @@
+package com.github.renuevo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGuideApplication.class, args);
+    }
+
+}
