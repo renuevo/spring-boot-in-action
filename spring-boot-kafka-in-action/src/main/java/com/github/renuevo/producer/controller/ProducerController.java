b6997a3148204ceed88d0b7c50544a1dea68a0b6
renuevo <renuevo0110@gmail.com>
2020-09-10 23:50:22 +0900

    Spring Kafka - Producer Async :sparkles:

--- spring-boot-kafka-in-action/src/main/java/com/github/renuevo/producer/controller/ProducerController.java
+++ spring-boot-kafka-in-action/src/main/java/com/github/renuevo/producer/controller/ProducerController.java
@@ -2,6 +2,7 @@ package com.github.renuevo.producer.controller;
 
 import com.github.renuevo.producer.service.KafkaService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,12 @@ public class ProducerController {
     @GetMapping("/push/msg")
     public void pushMassage(@RequestParam String msg) {
         this.kafkaService.sendMessage(msg);
+    }
+
+    @GetMapping("/push_async/msg")
+    public ResponseEntity<String> pushAsyncMassage(@RequestParam String msg) {
+        this.kafkaService.sendAsyncMessage(msg);
+        return ResponseEntity.ok("success");
     }
 
 }
--- spring-boot-kafka-in-action/src/main/java/com/github/renuevo/producer/service/KafkaService.java
+++ spring-boot-kafka-in-action/src/main/java/com/github/renuevo/producer/service/KafkaService.java
@@ -3,9 +3,10 @@ package com.github.renuevo.producer.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
-
-import java.util.concurrent.CompletableFuture;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -16,6 +17,21 @@ public class KafkaService {
 
     public void sendMessage(String msg) {
         kafkaTemplate.send("kafka-test-topic", msg);
+    }
+
+    public void sendAsyncMessage(String mag) {
+        ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send("kafka-test-topic", mag);
+        future.addCallback(new ListenableFutureCallback<>() {
+            @Override
+            public void onFailure(Throwable ex) {
+                log.error("Kafka Send Message Error : {} ", ex.getMessage(), ex);
+            }
+
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+                log.info("Kafka Send Message : {} / Offset : {}", mag, result.getRecordMetadata().offset());
+            }
+        });
     }
 
 }
