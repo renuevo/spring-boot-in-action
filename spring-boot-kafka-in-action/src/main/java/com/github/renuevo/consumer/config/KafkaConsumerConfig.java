bae29376ac64086304e277b6feab7575d3f4ba0b
renuevo <renuevo0110@gmail.com>
2020-09-20 23:37:31 +0900

    Spring Kafka - Create Model Consumer :sparkles:

--- spring-boot-kafka-in-action/src/main/java/com/github/renuevo/consumer/config/KafkaConsumerConfig.java
+++ spring-boot-kafka-in-action/src/main/java/com/github/renuevo/consumer/config/KafkaConsumerConfig.java
@@ -12,7 +12,6 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
-import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.Map;
 
