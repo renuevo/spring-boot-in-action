5e46412856d3108e4a6b568751cd917e0040601c
renuevo <renuevo0110@gmail.com>
2021-02-08 00:07:12 +0900

    Spring Rest Client - Add Reactive Feign Timeout :sparkles:

--- spring-boot-rest-client-in-action/src/main/java/com/github/renuevo/feign/config/ReactiveFeignConfig.java
+++ spring-boot-rest-client-in-action/src/main/java/com/github/renuevo/feign/config/ReactiveFeignConfig.java
@@ -3,8 +3,6 @@ package com.github.renuevo.feign.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactivefeign.ReactiveOptions;
-import reactivefeign.retry.BasicReactiveRetryPolicy;
-import reactivefeign.retry.ReactiveRetryPolicy;
 import reactivefeign.webclient.WebReactiveOptions;
 
 @Configuration
