d088be08a446ea85c57ad55858fe0a8b8064d778
renuevo <renuevo0110@gmail.com>
2021-02-09 02:18:13 +0900

    Spring Rest Client - Spring Version Upgrading :arrow_up:

--- spring-boot-rest-client-in-action/src/main/java/com/github/renuevo/feign/config/ReactiveFeignConfig.java
+++ spring-boot-rest-client-in-action/src/main/java/com/github/renuevo/feign/config/ReactiveFeignConfig.java
@@ -3,29 +3,32 @@ package com.github.renuevo.feign.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactivefeign.ReactiveOptions;
+import reactivefeign.retry.BasicReactiveRetryPolicy;
+import reactivefeign.retry.ReactiveRetryPolicy;
 import reactivefeign.webclient.WebReactiveOptions;
 
 @Configuration
 public class ReactiveFeignConfig {
 
     @Bean
-    public ReactiveOptions reactiveOptions(){
+    public ReactiveOptions reactiveOptions() {
         return new WebReactiveOptions.Builder()
                 .setReadTimeoutMillis(3000)
-                .setConnectTimeoutMillis(1000)
+                .setConnectTimeoutMillis(10000)
                 .build();
     }
 
     /**
-     *  {@link https://github.com/reactor/reactor}
-     *   this is required reactor-bom library `reactor.util.retry`
+     * {@link https://github.com/reactor/reactor}
+     * this is required reactor-bom library `reactor.util.retry` -> reactor-core 2.3.4
+     */
     @Bean
-    public ReactiveRetryPolicy reactiveRetryPolicy(){
+    public ReactiveRetryPolicy reactiveRetryPolicy() {
         return BasicReactiveRetryPolicy.retryWithBackoff(3, 5);
-    }*/
+    }
 
     @Bean
-    public feign.codec.ErrorDecoder reactiveStatusHandler(){
+    public feign.codec.ErrorDecoder reactiveStatusHandler() {
         return new FeignErrorDecoder();
     }
 }
