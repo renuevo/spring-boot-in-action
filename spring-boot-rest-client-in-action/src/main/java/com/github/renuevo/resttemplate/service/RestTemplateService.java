19b6d2a7bccd22521c52f876e9b927c963b9cb93
renuevo <renuevo0110@gmail.com>
2021-02-22 18:12:07 +0900

    Spring Rest Client - Default RestTemplateConfig

--- spring-boot-rest-client-in-action/src/main/java/com/github/renuevo/resttemplate/service/RestTemplateService.java
+++ spring-boot-rest-client-in-action/src/main/java/com/github/renuevo/resttemplate/service/RestTemplateService.java
@@ -1,7 +1,21 @@
 package com.github.renuevo.resttemplate.service;
 
+import com.github.renuevo.dto.NaverResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+@RequiredArgsConstructor
 @Service
 public class RestTemplateService {
+
+    @Value("${naver.url.search}")
+    private String url;
+    private final RestTemplate restTemplate;
+
+    public NaverResponse naverRestTemplateCall() {
+        return restTemplate.getForObject(url + "/{subUrl}", NaverResponse.class, "blog.json");
+    }
+
 }
