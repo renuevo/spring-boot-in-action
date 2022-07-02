e29c0d25bc282a25dc249f44264ea1d8ccd3b53b
renuevo <renuevo0110@gmail.com>
2022-07-02 23:41:10 +0900

    :ambulance: add redis repository key remove event process

--- spring-boot-redis-in-action/src/main/java/com/github/renuevo/config/RedisConfig.java
+++ spring-boot-redis-in-action/src/main/java/com/github/renuevo/config/RedisConfig.java
@@ -4,10 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableRedisRepositories(enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)   //해당설정이 없을시 redis key list 영구적으로 남음
 public class RedisConfig {
 
     @Bean
